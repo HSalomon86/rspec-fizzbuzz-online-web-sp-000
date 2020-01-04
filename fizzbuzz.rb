a6ad3b443cfa18c43fcc7cfa79074a02a32eed6c
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-04 22:27:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@
 def fizzbuzz(num)
   if (num % 3 == 0)
     return "Fizz"
-  elsif (num % 5 == 0) 
+  elsif (num % 5 == 0)
     puts "Buzz"
   elsif (num % 3 == 0) && (num % 5 == 0)
     puts "FizzBuzz"
